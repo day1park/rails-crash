31d4198bb7bb20f9032f769a1f7832ff41e82a03
day1park <daypark2408@gmail.com>
2019-02-23 15:24:25 +1100

    create private method to take in params, redirect to a show post after creating and saving new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,11 +3,24 @@ class PostsController < ApplicationController
 
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   def new
 
   end
 
   def create
     # render plain: params[:post].inspect 
+    @post = Post.new(post_params)
+
+    @post.save
+    redirect_to @post #redirect to show view
+  end
+
+  #private method with the params the post will take
+  private def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
