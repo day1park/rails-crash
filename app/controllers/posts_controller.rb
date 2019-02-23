6d18075f967dcd2f26734026abf20fc226433987
day1park <daypark2408@gmail.com>
2019-02-23 16:34:04 +1100

    create a destroy post method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,14 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
   #private method with the params the post will take
   private def post_params
     params.require(:post).permit(:title, :body)
